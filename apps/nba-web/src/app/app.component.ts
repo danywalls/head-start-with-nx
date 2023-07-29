a4d65e17950a2faab34a6033e2adf17c79a442c1
Dany Paredes <danywalls4@gmail.com>
2023-07-29 09:43:34 +0200

    setup default base

--- apps/nba-web/src/app/app.component.ts
+++ apps/nba-web/src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { NavigationComponent } from '@nba/nba-ui';
+
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NavigationComponent],
   selector: 'nba-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
